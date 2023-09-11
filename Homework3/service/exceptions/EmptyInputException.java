298790b23ca8d66faee07d323b8b72ca52157b22
Viki <viki333@list.ru>
2023-09-11 13:08:20 +0300

    New  fail

---
+++ Homework3/service/exceptions/EmptyInputException.java
@@ -0,0 +1,9 @@
+package Homework3.service.exceptions;
+
+import java.io.IOException;
+
+public class EmptyInputException extends IOException {
+    public EmptyInputException() {
+        super("Ввод не может быть пустым.");
+    }
+}
